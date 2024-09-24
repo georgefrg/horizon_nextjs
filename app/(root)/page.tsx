61b119028faefee90ae4501d2431489bf2a45b22
georgefrg <georgeftw00@gmail.com>
2024-09-24 23:38:57 +0300

    implement Doughnut chart and animation for total Balance

---
+++ app/(root)/page.tsx
@@ -0,0 +1,29 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
+
+const Home = () => {
+  const loggedIn = { firstName: "george" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={10000.43}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
